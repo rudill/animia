f6f5282670d2ff19166fa3c9c4028d3bb771d7b5
Prasanna BMRD <bmruvinda2001@gmail.com>
2025-03-05 13:23:05 +0530

    Implement custom dark theme for improved UI

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final ThemeData customTheme = ThemeData(
+      colorScheme: const ColorScheme.dark(
+        primary: Color(0xFF0D1622),
+        onPrimary: Colors.white,
+        surface: Color(0xFF0D1622),
+        onSurface: Colors.white,
+      ),
+      scaffoldBackgroundColor: const Color(0xFF0D1622),
+      textTheme:  const TextTheme(
+        bodyLarge: TextStyle(color: Colors.white, fontSize: 18),
+        bodyMedium: TextStyle(color: Colors.white70, fontSize: 16),
+        displayLarge: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      buttonTheme: const ButtonThemeData(
+        buttonColor: Color(0xFF0D1622),
+        textTheme: ButtonTextTheme.primary,
+      ),
+    );
+
+
     return MaterialApp(
-        theme: ThemeData(brightness: Brightness.dark),
+        theme: customTheme,
         home:  const Scaffold(
           body: HomeScreen(),
         ));
